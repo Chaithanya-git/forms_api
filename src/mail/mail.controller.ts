6e4102422f168e7aa9155b50f62ef1eb364bdef0
chaithanya-git <chaithanya-git@github.com>
2023-08-20 20:17:23 -0500

    added swagger

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { MailService } from './mail.service';
 import { CreateMailDto } from './dto/create-mail.dto';
 import { SendMailDto } from './dto/send-mail.dto';
@@ -10,11 +10,19 @@ export class MailController {
   constructor(private readonly mailService: MailService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'The mail has been successfully created.',
+  })
   addMail(@Body() createMailDto: CreateMailDto) {
     return this.mailService.addMail(createMailDto);
   }
 
   @Post('send')
+  @ApiResponse({
+    status: 200,
+    description: 'The mail has been successfully sent.',
+  })
   sendMail(@Body() sendMailDto: SendMailDto) {
     return this.mailService.sendMail(sendMailDto);
   }
